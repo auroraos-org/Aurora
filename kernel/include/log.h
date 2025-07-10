bc70c473f097c0f7e523d68706bc92a2624481d8
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-07-10 07:39:20 +0200

    Fix

--- kernel/include/log.h
+++ kernel/include/log.h
@@ -31,6 +31,25 @@ static inline void log_print(const char* str) {
     }
 }
 
+static inline void log_print_hex(u32 value) {
+    // TODO: THIS IS SOOOO BAD CODE
+    char hex_str[11];
+    char hex_chars[] = "0123456789ABCDEF";
+    int i;
+    
+    // 0x prefix
+    hex_str[0] = '0';
+    hex_str[1] = 'x';
+    
+
+    for (i = 7; i >= 0; i--) {
+        hex_str[2 + (7 - i)] = hex_chars[(value >> (i * 4)) & 0xF];
+    }
+    
+    hex_str[10] = '\0'; 
+    log_print(hex_str);
+}
+
 #define LOG_ERROR(message) log_print("\033[31m [ERROR] \033[0m " message)
 #define LOG_INFO(message) log_print("\033[96m [INFO] \033[0m " message)
 #define LOG_WARNING(message) log_print("\033[95m [WARNING] \033[0m " message)
